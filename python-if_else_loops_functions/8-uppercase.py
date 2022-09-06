23428ea3e94faa475f0c2ec95cecb0033389e585
Damian Caetano <5100@holbertonschool.com>
2022-09-06 12:06:47 -0700

    task 7 and task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        x = ord(x)
+        if 97 <= x <= 122:
+            x = x - 32
+        print("{:c}".format(x), end="")
+    pritn()
