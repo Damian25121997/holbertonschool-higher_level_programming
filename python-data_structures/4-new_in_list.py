7add553a06c35536ebb33a430411cae4d5993c6e
Damian Caetano <5100@holbertonschool.com>
2022-09-12 06:39:32 -0700

    task 4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return
+    new_list = list.copy(my_list)
+    if 0 <= idx < len(my_list):
+        new_list[idx] = element
+    return new_list
