c2693d427ebb99479026fa51152361318581162a
Damian Caetano <5100@holbertonschool.com>
2022-09-12 06:02:46 -0700

    fix function 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    if idx > len(my_list) > 0:
-        return None
-    else:
+    if idx > len(my_list) >= 0:
         return my_list[idx]
+    else:
+        return None
