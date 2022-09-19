badea2b83065e2954d6084ff26063e60e69302fb
Damian25121997 <damiancaetano25@gmail.com>
2022-09-19 12:30:23 -0300

    add function 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square():
+    """Class Square that defines a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size to a value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value)!= 2:
+            raise TypeError("position must be a tuple of 2 positive integrers")
+    
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints to stdout the square with the character #,
+        at the position given by the position attribute.
+        """
+        if  self.__size == 0:
+            print()
+            return
+        for x in range(0, self.__position[1]):
+            print()
+        for z in range(0, self.__size):
+            for i in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(0, self.__size):
+                print("#", end="")
+            print()
