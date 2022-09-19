04a5ec635d946d74c867285ac7f10a01e79c32b9
Damian25121997 <damiancaetano25@gmail.com>
2022-09-19 13:37:24 -0300

    fix task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square():
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the data."""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -31,12 +31,13 @@ class Square():
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
-           type(value[1]) is not int or value[1] < 0:
+        if type(value) is not tuple or len(value) != 2:
+            ("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or value[0] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        if type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Return the current square area."""
