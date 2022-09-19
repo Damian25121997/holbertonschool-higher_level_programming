ae93abe8197d61d4663635512a784490155b31ce
Damian25121997 <damiancaetano25@gmail.com>
2022-09-19 12:42:16 -0300

    fix task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,7 +37,8 @@ class Square():
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
         """Return the current square area."""
