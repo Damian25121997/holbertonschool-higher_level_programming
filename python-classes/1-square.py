c1c2faf8defb7678e1e2a0498eb6719841b99f5f
Damian25121997 <damiancaetano25@gmail.com>
2022-09-19 11:15:09 -0300

    add  function of the task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square():
+    """Class Square that defines a square."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
