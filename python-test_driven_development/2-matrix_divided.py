99aef307da629ff847998ef01eb3fd2d9e437186
Damian25121997 <damiancaetano25@gmail.com>
2022-09-21 17:51:26 -0300

    fix error of the function 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,13 +16,17 @@ def matrix_divided(matrix, div):
         raise TypeError("division by zero")
     new_matrix = []
     for element in matrix:
-        if len(0) != len(str(element)):
-            raise TypeError("Each row of the matrix must have the same size")
+        if type(element) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         new_list = []
         for i in element:
-            if type(i) != int and type(i) != float:
+            if type(i) is not int and type(i) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
             x = i / div
             new_list.append(round(x, 2))
         new_matrix.append(new_list)
+    len_list = len(matrix[0])
+    for r in matrix:
+        if len(r) != len_list:
+            raise TypeError("Each row of the matrix must have the same size")
     return new_matrix
