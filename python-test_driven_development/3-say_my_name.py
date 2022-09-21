c65d9c09219d91084c376b3c5382938021395a23
Damian25121997 <damiancaetano25@gmail.com>
2022-09-21 19:07:26 -0300

    add function 3 and test 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define the prin the first name and last name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function divides all elements of a matrix.
+    Args:
+        first_name: is a name.
+        last_name: is a last name.
+    Return:
+        Always.
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
