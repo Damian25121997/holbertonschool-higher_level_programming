32f3c50dc6eeb70bc4c1c88537a736cf4088a9a1
Damian25121997 <damiancaetano25@gmail.com>
2022-09-21 20:48:24 -0300

    git add function 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])"""
+
+import unittest
+
+
+import unittest
+max_integer = __import('6-max_integer').max_integer
+
+class Test_Cases(unittest.TestCase):
+
+    def test_results(self):
+        self.assertAlmostEqual(max_integer([], None))
+        self.assertAlmostEqual(max_integer([0, 2, 3], 3)
+        self.assertAlmostEqual(max_integer(([-1, 0, 10, 1000]), 1000)
+        self.assertAlmostEqual(max_integer(([-50, -30, -10, 0]), 0)
