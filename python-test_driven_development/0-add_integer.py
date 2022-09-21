bec1979faeec1e42427c8fd273d6c2bfdeeec7cb
Damian25121997 <damiancaetano25@gmail.com>
2022-09-21 10:56:29 -0300

    fix tfunction of task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,9 @@ def add_integer(a, b=98):
     Return:
         int: The return value of result of add.
     """
-    if type(a) != int and type(a) != float:
+    if type(a) != int and type(a) != float or a != a:
         raise TypeError("a must be an integer")
-    if type(b) != int and type(b) != float:
+    if type(b) != int and type(b) != float or b != b:
         raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
