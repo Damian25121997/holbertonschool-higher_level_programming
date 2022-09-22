fa6a663f109da21feef2d237f4b4303f3bc9bde6
Damian25121997 <damiancaetano25@gmail.com>
2022-09-22 10:20:57 -0300

    add function 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Class Rectangle that defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initializes the data."""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Retieves the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width to a value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retieves the heigth"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the heigth to a value"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return current rectangle area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Return current rectangle perimeter"""
+        return 2 * (self.__width + self.__height)
