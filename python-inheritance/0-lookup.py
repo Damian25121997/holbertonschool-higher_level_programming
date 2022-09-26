22d923b3f0075aa41cd7fc8c546b911217f0033f
Damian25121997 <damiancaetano25@gmail.com>
2022-09-26 10:45:35 -0300

    fix function 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -4,5 +4,10 @@ and methods of an object"""
 
 
 def lookup(obj):
+    """Return the list of acaliable attributes and methods
+    of an object
+    
+    obj: is an object
+    """
     new_list = dir(obj)
     return list(new_list)
