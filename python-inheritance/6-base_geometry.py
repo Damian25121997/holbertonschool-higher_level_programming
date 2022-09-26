24b52d4fae511cba4a07c63dbc3693ca98a71d61
Damian25121997 <damiancaetano25@gmail.com>
2022-09-26 13:52:05 -0300

    add function 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write an empty class"""
+
+
+class BaseGeometry:
+    """Class"""
+
+    def area(self):
+        """Raise an Exception error"""
+        raise Exception("area() is not implemented")
