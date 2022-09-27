82bb38df2e7e7bb65fbcaf7baefa19797af4fd8e
Damian25121997 <damiancaetano25@gmail.com>
2022-09-27 12:19:12 -0300

    fix function 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Function thats appends a string to a text file"""
+"""Function thats appends a string to a text file."""
 
 
 def write_file(filename="", text=""):
-    """Function appends a string to a text file"""
+    """Function appends a string to a text file."""
 
     with open(filename, 'a+') as file:
         return file.write(text)
