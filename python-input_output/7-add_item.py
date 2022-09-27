779ee7b8af3ae24b9dbd9066bc3786834f71a821
Damian Caetano <5100@holbertonschool.com>
2022-09-27 12:00:23 -0700

    add new function 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Function adds all arguments to a Python list"""
+
+
+import sys
+import os.path
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+file = 'add_item.json'
+myList = []
+
+if os.path.exists(file) and os.path.getsize(file) > 0:
+    myList = load_from_json_file(file)
+
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        myList.append(arg)
+
+save_to_json_file(myList, file)
