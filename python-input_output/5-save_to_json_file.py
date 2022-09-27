e41af8b06ec79da6828f6b5ba991062b11a7c9a1
Damian25121997 <damiancaetano25@gmail.com>
2022-09-27 12:50:01 -0300

    add function 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that writes an Object to a text file"""
+
+
+from asyncore import write
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Function that writes an Object to a text file"""
+
+    with open(filename, 'w+') as file:
+        json.dump(my_obj, file)
