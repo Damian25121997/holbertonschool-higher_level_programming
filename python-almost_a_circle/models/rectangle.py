fc3d477c454ce5ca37bd965cd6efd6a65fe210b1
Damian25121997 <damiancaetano25@gmail.com>
2022-10-01 17:06:46 -0300

    Add model Rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defina a class Rectangle"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes a Rectangle"""
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
