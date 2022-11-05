61bbf0a9230f92ba913535aa499803464261d3fa
Damian Caetano <5100@holbertonschool.com>
2022-11-04 21:00:00 -0700

    Fix task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,8 +1,4 @@
 -- List all records of the table of the database in MySQL server.
-INSERT INTO second_table (name, score)
-VALUES ('Aria', 18);
-INSERT INTO second_table (name, score)
-VALUES ('Aria', 12);
 SELECT score, name
 FROM second_table
 WHERE `name` IS NOT NULL AND score >= 10
