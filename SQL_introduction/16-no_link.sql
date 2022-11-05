f25f7f9a3fbf4f92f7bcd7813a1350e121bfc290
Damian Caetano <5100@holbertonschool.com>
2022-11-04 21:00:54 -0700

    Fix task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- List all records of the table of the database in MySQL server.
 SELECT score, name
 FROM second_table
-WHERE `name` IS NOT NULL AND score >= 10
+WHERE `name` IS NOT NULL
 ORDER BY score DESC;
