41ea19c5faeeab1ca6a07bfa64c6acf6d725a4f7
Damian Caetano <5100@holbertonschool.com>
2022-11-04 20:45:21 -0700

    Fix task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- List the number of records with the same score in the table of rhe database in MySQL server.
-SELECT score, COUNT(*) AS 'number'
+SELECT score, COUNT(score) AS 'number'
 FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY score DESC;
