0956f0a7d1d8e6171c724daa3888ac3e588d4f93
Damian <damiancaetano25@gmail.com>
2022-12-06 02:03:56 +0000

    add task 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// The script display in console
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < array.length; index++) {
+  console.log(array[index]);
+}
