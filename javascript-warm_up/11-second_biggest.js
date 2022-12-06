42faef397183742e3fff467cc1a17447ca917689
Damian <damiancaetano25@gmail.com>
2022-12-06 17:06:21 +0000

    fix task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,22 @@
 #!/usr/bin/node
 
 // The script display in console
-const arg = process.argv;
-const array = [];
-for (let x = 2; arg[x]; x++) {
-  array.push(arg[x]);
+const arg = process.argv
+let array = []
+for (let x = 2;arg[x]; x++) {
+    array.push(parseInt(arg[x]));
 }
-array.sort();
-array.reverse();
-let x = 0;
-if (array.length === 0 || array.length === 1) {
-  console.log(x);
+array.sort( function(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+})
+array.reverse()
+let x = 0
+if (array.length === 0 || array.length === 1)
+{
+    console.log(x)
 } else {
-  x = array[1];
-  console.log(x);
+    x = array[1]
+    console.log(x)
 }
