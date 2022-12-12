d096879d3e2a9188c976596ba510436049d4d1ec
Damian <damiancaetano25@gmail.com>
2022-12-12 23:24:02 +0000

    Add task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (error, data, body) {
+  fs.writeFile(file, body, 'utf8', function (error_) {
+    if (error_) console.log(error_);
+  });
+});
