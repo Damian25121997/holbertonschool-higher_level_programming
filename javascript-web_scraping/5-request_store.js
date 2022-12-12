69adcf0fee81cb16dbf7f1a7a8fb06b3eaa29654
Damian <damiancaetano25@gmail.com>
2022-12-12 23:25:24 +0000

    fix task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,6 +6,7 @@ const url = process.argv[2];
 const file = process.argv[3];
 
 request(url, function (error, data, body) {
+  if (error) console.log(error);
   fs.writeFile(file, body, 'utf8', function (error_) {
     if (error_) console.log(error_);
   });
